80c48bb37fd59ab5ff48487d4c9a2ba37b4a330d
Datla Rohit Varma <visaal1582892@gmail.com>
2025-04-28 12:22:54 +0530

    made some changes

--- vite.config.js
+++ vite.config.js
@@ -10,15 +10,15 @@ export default defineConfig({
   ],
 
   // Needed only for development
-  server: {
-    host: true,
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'https://freetestapi.com',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api')
-      }
-    }
-  },
+  // server: {
+  //   host: true,
+  //   port: 5173,
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://freetestapi.com',
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, '/api')
+  //     }
+  //   }
+  // },
 })
